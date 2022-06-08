03386958b64421e3a5504b7b8eb97ae6e158c9e5
LazyDeveloperss <107105531+LazyDeveloperss@users.noreply.github.com>
2022-06-08 18:25:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/LazyDeveloperss/test9.git /test9
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
