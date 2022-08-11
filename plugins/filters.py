e0a95a514f7d9ce26b53d73d5320f3085fe85ecc
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2022-08-11 20:40:26 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -198,7 +198,7 @@ async def deletefilter(client, message):
                 await message.reply_text("Make sure I'm present in your group!!", quote=True)
                 return
         else:
-            await message.reply_text("I'm not connected to any groups!", quote=True)
+            await message.reply_text("please use /delete to delete filter manually 🥴", quote=True)
 
     elif chat_type in ["group", "supergroup"]:
         grp_id = message.chat.id
