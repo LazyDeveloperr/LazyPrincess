5aa70b2091ddd53f0cf7e7bbc7a49df6d23ae9fd
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2022-10-24 12:11:31 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,7 +4,7 @@ My name is <a href=https://t.me/{}>{}</a>, Sona Main 999999kmps ke speed se movi
     HELP_TXT = """𝙷𝙴𝚈 {}
 Here is the help for my COMMANDS."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/LazyDevelopers>LazyDevelopers</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/LazyDeveloper>LazyDeveloper</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
@@ -15,7 +15,7 @@ Here is the help for my COMMANDS."""
 - Source - https://github.com/LazyDeveloperr/LazyPrincess  
 
 <b>DEVS:</b>
-- <a href=https://t.me/LazyDevelopers>LazyDevelopers</a>"""
+- <a href=https://t.me/LazyDeveloper>LazyDeveloper</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and LazyPriness will respond whenever that keyword hits the message
@@ -40,7 +40,7 @@ Here is the help for my COMMANDS."""
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/LazyDevelopers)</code>
+<code>[Button Text](buttonurl:https://t.me/LazyDeveloper)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
