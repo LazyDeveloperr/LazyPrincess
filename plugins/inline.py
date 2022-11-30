9625b829df315970247033d18478a85217363e27
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2022-11-30 20:34:33 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -26,7 +26,7 @@ async def answer(bot, query):
     if not await inline_users(query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='okDa',
+                           switch_pm_text='Results',
                            switch_pm_parameter="hehe")
         return
 
