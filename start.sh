a02ab6512e08ff61f0ba6c7c855873a9e5fd3758
Sudharshan12467 <110244004+Sudharshan12467@users.noreply.github.com>
2022-12-14 17:20:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LazyDeveloperr/LazyPrincess.git /LazyPrincess
+  git clone https://github.com/Sudharshan1246/LazyPrincess.git /Team_MCU 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  git clone $UPSTREAM_REPO /Team_MCU
 fi
 cd /LazyPrincess
 pip3 install -U -r requirements.txt
