67f8b17377a859286491fb91cf7299ecd1014e05
Sudharshan12467 <110244004+Sudharshan12467@users.noreply.github.com>
2022-12-14 18:39:19 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -40,10 +40,10 @@ async def gen_link_s(bot, message):
 @Client.on_message(filters.command(['batch', 'pbatch']) & filters.create(allowed))
 async def gen_link_batch(bot, message):
     if " " not in message.text:
-        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/LazyDeveloper https://t.me/LazyDeveloper</code>.")
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/Team_MCU https://t.me/Team_MCU</code>.")
     links = message.text.strip().split(" ")
     if len(links) != 3:
-        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/LazyDeveloper https://t.me/LazyDeveloper</code>.")
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/Team_MCU https://t.me/Team_MCU</code>.")
     cmd, first, last = links
     regex = re.compile("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
     match = regex.match(first)
