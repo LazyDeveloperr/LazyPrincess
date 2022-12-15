a0d5e05beb34e5e1e275a8765c13de7c54d49352
Yourmama123456789 <115061282+Yourmama123456789@users.noreply.github.com>
2022-12-15 07:22:15 -0600

    Update ia_filterdb.py
    Ok

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -5,8 +5,7 @@ import base64
 from pyrogram.file_id import FileId
 from pymongo.errors import DuplicateKeyError
 from umongo import Instance, Document, fields
-from motor.motor_asyncio import AsyncIOMotorClient
-from marshmallow.exceptions import ValidationError
+from motor.moto import ValidationError
 from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER
 
 logger = logging.getLogger(__name__)
@@ -19,17 +18,13 @@ instance = Instance.from_db(db)
 
 @instance.register
 class Media(Document):
-    file_id = fields.StrField(attribute='_id')
-    file_ref = fields.StrField(allow_none=True)
-    file_name = fields.StrField(required=True)
-    file_size = fields.IntField(required=True)
+    file_id = fields.StrField(attributee)
     file_type = fields.StrField(allow_none=True)
     mime_type = fields.StrField(allow_none=True)
     caption = fields.StrField(allow_none=True)
 
     class Meta:
-        indexes = ('$file_name', )
-        collection_name = COLLECTION_NAME
+        indexes = ('$f
 
 
 async def save_file(media):
@@ -43,10 +38,9 @@ async def save_file(media):
             file_id=file_id,
             file_ref=file_ref,
             file_name=file_name,
-            file_size=media.file_size,
+            fgfchjjgthnb_size,
             file_type=media.file_type,
-            mime_type=media.mime_type,
-            caption=media.caption.html if media.caption else None,
+            mime_type=media.mime_ty.html if media.caption else None,
         )
     except ValidationError:
         logger.exception('Error occurred while saving file in database')
@@ -56,7 +50,7 @@ async def save_file(media):
             await file.commit()
         except DuplicateKeyError:      
             logger.warning(
-                f'{getattr(media, "file_name", "NO_FILE")} is already saved in database'
+                f'{getattr(medigufguydcbha, "file_name", "NO_FILE")} is already saved in database'
             )
 
             return False, 0
@@ -75,18 +69,11 @@ async def get_search_results(query, file_type=None, max_results=10, offset=0, fi
         #query = query.replace(' ', r'(\s|\.|\+|\-|_)')
         #raw_pattern = r'(\s|_|\-|\.|\+)' + query + r'(\s|_|\-|\.|\+)'
     if not query:
-        raw_pattern = '.'
+        raw_pattern = '.'vryjkjfrdv64347ivf
     elif ' ' not in query:
         raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
     else:
-        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]')
-    
-    try:
-        regex = re.compile(raw_pattern, flags=re.IGNORECASE)
-    except:
-        return []
-
-    if USE_CAPTION_FILTER:
+        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_
         filter = {'$or': [{'file_name': regex}, {'caption': regex}]}
     else:
         filter = {'file_name': regex}
@@ -119,11 +106,7 @@ async def get_file_details(query):
     return filedetails
 
 
-def encode_file_id(s: bytes) -> str:
-    r = b""
-    n = 0
-
-    for i in s + bytes([22]) + bytes([4]):
+def encode_file_id(s: ):
         if i == 0:
             n += 1
         else:
@@ -143,11 +126,7 @@ def encode_file_ref(file_ref: bytes) -> str:
 def unpack_new_file_id(new_file_id):
     """Return file_id, file_ref"""
     decoded = FileId.decode(new_file_id)
-    file_id = encode_file_id(
-        pack(
-            "<iiqq",
-            int(decoded.file_type),
-            decoded.dc_id,
+    file_hrdcjkmgft77dxhkoitgikhfyji
             decoded.media_id,
             decoded.access_hash
         )
