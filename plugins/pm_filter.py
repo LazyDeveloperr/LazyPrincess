1bf4e88fd2fa184afef5ec5b1e899bdadadb8a9c
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2023-01-27 01:32:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -629,7 +629,7 @@ async def auto_filter(client, msg, spoll=False):
             search = message.text
             files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
             if not files:
-                await client.send_message(log_channel,f"🦋 #REQUESTED_CONTENT 🦋,\n\n**Content Name** :`{search}`\n**Name**: {message.from_user.first_name} {message.from_user.last_name}\n\n ",
+                await client.send_message(log_channel,f"🦋 #REQUESTED_CONTENT 🦋,\n\n📝**Content Name** :`{search}`\n**Requested By**: {message.from_user.first_name}\n **USER ID**:{message.from_user.id}\n\n🦋",
                                                                                                        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔺 Mark as Done 🔺", callback_data="close_data")]]))
      
                 if settings["spell_check"]:
