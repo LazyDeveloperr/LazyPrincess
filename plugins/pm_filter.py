ac08949556c29e04b3f2451171949d5b799e63df
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2023-03-05 18:18:51 +0530

    Added Some extra buttons

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -226,11 +226,13 @@ async def next_page(bot, query):
             for file in files
         ]
 
-    btn.insert(1,
+    btn.insert(0,
         [
             InlineKeyboardButton(text="Movie Channel", url='https://telegram.me/real_MoviesAdda2'),
             InlineKeyboardButton(text="Updates", url='https://telegram.me/LazyDeveloper')
-        ],
+        ]
+    )
+    btn.insert(1,
         [
             InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://telegram.me/LazyDeveloper')
         ]
@@ -933,11 +935,13 @@ async def auto_filter(client, msg, spoll=False):
             for file in files
         ]
 
-    btn.insert(1,
+    btn.insert(0,
         [
             InlineKeyboardButton(text="Movie Channel", url='https://telegram.me/real_MoviesAdda2'),
             InlineKeyboardButton(text="Updates", url='https://telegram.me/LazyDeveloper')
-        ],
+        ]
+    )
+    btn.insert(1,
         [
             InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://telegram.me/LazyDeveloper')
         ]
