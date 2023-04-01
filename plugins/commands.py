655bfe025dac1e7a7f573e099f237f370a6fa1d6
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-04-01 14:30:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -45,7 +45,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('◽️ᴏᴡɴᴇʀ', url='https://t.me/GT_ben),
+            InlineKeyboardButton('◽️ᴏᴡɴᴇʀ', url='https://t.me/GT_ben'),
             InlineKeyboardButton('◽️ᴍᴏᴠɪᴇ ɢʀᴏᴜᴩ', url='https://t.me/+9CKK8DlZlgUxOTE9')
             ],[
             InlineKeyboardButton('◽️ʜᴇʟᴩ', callback_data='help'),
