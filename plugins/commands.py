7687d7d3bbcaf4b8e84eaa7f603aafdbdffaae14
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-04-12 07:39:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -245,7 +245,6 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup(buttons),
         protect_content=True if pre == 'filep' else False,
         )
                     
