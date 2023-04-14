6b0f925d22e6137b4d4f1d5f24d79c5e2db352f0
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-04-14 07:18:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -45,7 +45,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [
-                [Inlinekeyboardbutton('⚡ ᴄʟɪᴄᴋ ʜᴇʀᴇ ꜰᴏʀ ᴍᴏʀᴇ ʙᴜᴛᴛᴏɴꜱ ⚡️', callback_data="about")]
+                [Inlinekeyboardbutton('⚡ ᴄʟɪᴄᴋ ʜᴇʀᴇ ꜰᴏʀ ᴍᴏʀᴇ ʙᴜᴛᴛᴏɴꜱ ⚡️', callback_data="start")]
         ]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
