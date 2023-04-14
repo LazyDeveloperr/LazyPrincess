2c33ded7d9cc7f4d4323684742ac6b20ca3c6b21
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-04-14 07:08:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -44,13 +44,9 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
-        buttons = [[
-            InlineKeyboardButton('◽️ᴏᴡɴᴇʀ', url='https://t.me/GT_ben'),
-            InlineKeyboardButton('◽️ᴍᴏᴠɪᴇ ɢʀᴏᴜᴩ', url='https://t.me/+9CKK8DlZlgUxOTE9')
-            ],[
-            InlineKeyboardButton('◽️ʜᴇʟᴩ', callback_data='help'),
-            InlineKeyboardButton('◽️ᴀʙᴏᴜᴛ', callback_data='about')
-        ]]
+        buttons = [
+                [Inlinekeyboardbutton('⚡ ᴄʟɪᴄᴋ ʜᴇʀᴇ ꜰᴏʀ ᴍᴏʀᴇ ʙᴜᴛᴛᴏɴꜱ ⚡️', callback_data="about")]
+        ]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
