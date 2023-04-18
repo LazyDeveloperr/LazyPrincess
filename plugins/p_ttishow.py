e43b6d22f7c5fde8d3bac42737318ec13a785048
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-04-18 09:50:29 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -53,12 +53,12 @@ async def save_group(bot, message):
                         await (temp.MELCOW['welcome']).delete()
                 except:
                     pass
-            if WELCOM_PIC:
-                temp.MELCOW['welcome'] = await message.reply_photo(photo=WELCOM_PIC, caption=WELCOM_TEXT.format(user=u.mention, chat=message.chat.title),
-                                                                   reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🇦🇺 ᴇɴɢ", callback_data="engwlc"),InlineKeyboardButton("🇮🇳 ʜɴᴅ", callback_data="hndwlc"),InlineKeyboardButton("🇸🇦 ᴀʀʙ", callback_data="arbwlc")],[InlineKeyboardButton("📍 ʀᴜʟᴇꜱ 📍", url="http://t.me/DrMasterTGbot?start=regole_-1001397638909")]]))
-            else:
-                temp.MELCOW['welcome'] = await message.reply_text(text=WELCOM_TEXT.format(user=u.mention, chat=message.chat.title),
-                                                                  reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🇦🇺 ᴇɴɢ", callback_data="engwlc"),InlineKeyboardButton("🇮🇳 ʜɴᴅ", callback_data="hndwlc"),InlineKeyboardButton("🇸🇦 ᴀʀʙ", callback_data="arbwlc")],[InlineKeyboardButton("📍 ʀᴜʟᴇꜱ 📍", url="http://t.me/DrMasterTGbot?start=regole_-1001397638909")]]))
+                if WELCOM_PIC:
+                    temp.MELCOW['welcome'] = await message.reply_photo(photo=WELCOM_PIC, caption=WELCOM_TEXT.format(user=u.mention, chat=message.chat.title),
+                                                                       reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🇦🇺 ᴇɴɢ", callback_data="engwlc"),InlineKeyboardButton("🇮🇳 ʜɴᴅ", callback_data="hndwlc"),InlineKeyboardButton("🇸🇦 ᴀʀʙ", callback_data="arbwlc")],[InlineKeyboardButton("📍 ʀᴜʟᴇꜱ 📍", url="http://t.me/DrMasterTGbot?start=regole_-1001397638909")]]))
+                else:
+                    temp.MELCOW['welcome'] = await message.reply_text(text=WELCOM_TEXT.format(user=u.mention, chat=message.chat.title),
+                                                                      reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🇦🇺 ᴇɴɢ", callback_data="engwlc"),InlineKeyboardButton("🇮🇳 ʜɴᴅ", callback_data="hndwlc"),InlineKeyboardButton("🇸🇦 ᴀʀʙ", callback_data="arbwlc")],[InlineKeyboardButton("📍 ʀᴜʟᴇꜱ 📍", url="http://t.me/DrMasterTGbot?start=regole_-1001397638909")]]))
 
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
