fd3d6dd747eb692a6a7e5f23ab002e73f512c0ff
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-04-18 09:02:44 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -55,7 +55,7 @@ async def save_group(bot, message):
                     pass
             if WELCOM_PIC:
                 temp.MELCOW['welcome'] = await message.reply_photo(photo=WELCOM_PIC, caption=WELCOM_TEXT.format(user=u.mention, chat=message.chat.title),
-                                                                   reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🇦🇺 ᴇɴɢ", callback_data="engwlc"),InlineKeyboardButton("🇮🇳 ʜɴᴅ", callback_data="hndwlc"),InlineKeyboardButton("🇸🇦 ᴀʀʙ", callback_data="arbwlc")]]))
+                                                                   reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🇦🇺 ᴇɴɢ", callback_data="engwlc"),InlineKeyboardButton("🇮🇳 ʜɴᴅ", callback_data="hndwlc"),InlineKeyboardButton("🇸🇦 ᴀʀʙ", callback_data="arbwlc")],[InlineKeyboardButton("📍 ʀᴜʟᴇꜱ 📍", url="http://t.me/DrMasterTGbot?start=regole_-1001397638909")]]))
             else:
                 temp.MELCOW['welcome'] = await message.reply_text(text=WELCOM_TEXT.format(user=u.mention, chat=message.chat.title),
                                                                   reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("hi", callback_data="close_data")]]))
