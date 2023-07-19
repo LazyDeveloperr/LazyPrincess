dc8a2fab8d12ed9487ac60efb3f4f1b0d3ac45ff
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-07-19 22:05:08 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -55,7 +55,7 @@ async def start(client, message):
             InlineKeyboardButton('🙆🏻 Help 🦾', callback_data='help'),
             InlineKeyboardButton('♥️ About ♥️', callback_data='about')
             ],[
-            InlineKeyboardButton('Watch Tutorial', url='https://youtube.com/LazyDeveloper')
+            InlineKeyboardButton('Watch Tutorial', url='https://youtube.com/@moviekingdom522')
             ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
