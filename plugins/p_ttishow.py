806cffccc91753eac70d30780294e778edde08be
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-08-05 23:53:27 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -8,7 +8,7 @@ from utils import get_size, temp, get_settings
 from Script import script
 from pyrogram.errors import ChatAdminRequired
 
-"""-----------------------------------------https://t.me/+yyijzRAlsCszZDdl --------------------------------------"""
+"""-----------------------------------------https://t.me/Infinity_XBotz --------------------------------------"""
 
 @Client.on_message(filters.new_chat_members & filters.group)
 async def save_group(bot, message):
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('🤥 Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('🔔 Updates', url='https://t.me/+yyijzRAlsCszZDdl')
+            InlineKeyboardButton('🔔 Updates', url='https://t.me/Infinity_XBotz')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
