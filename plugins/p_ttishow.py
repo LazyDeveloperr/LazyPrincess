6c4bbc85adb0d18375997839ab8403c7cb24e2ec
Pc <126058657+PCJOD@users.noreply.github.com>
2023-09-07 08:10:13 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('🤥 Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('🔔 Updates', url='https://t.me/LazyDeveloper')
+            InlineKeyboardButton('🔰 Movie Group 🔰', url='https://t.me/pcmoviegroup')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
