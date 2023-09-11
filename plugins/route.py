588caf3c836b36b7ed8164e206e59515d5f86e4a
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-09-11 16:45:07 +0530

    Delete plugins directory

--- plugins/route.py
+++
@@ -1,7 +0,0 @@
-from aiohttp import web
-
-routes = web.RouteTableDef()
-
-@routes.get("/", allow_head=True)
-async def root_route_handler(request):
-    return web.json_response("in & as LazyDeveloper...")
