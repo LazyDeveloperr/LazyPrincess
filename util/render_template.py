d11b6b49f5d2707c124b7ab72d1ef291d6203398
Lazy Developer <107105531+LazyDeveloperr@users.noreply.github.com>
2023-12-15 19:06:24 +0530

    Update render_template.py

--- util/render_template.py
+++ util/render_template.py
@@ -5,49 +5,39 @@
 # 🥰  Thank you for giving me credit @LazyDeveloperr  🥰
 # for any error please contact me -> telegram@LazyDeveloperr or insta @LazyDeveloperr 
 # rip paid developers 🤣 - >> No need to buy paid source code while @LazyDeveloperr is here 😍😍
-
-import jinja2
 from info import *
 from lazybot import LazyPrincessBot
 from util.human_readable import humanbytes
 from util.file_properties import get_file_ids
 from server.exceptions import InvalidHash
 import urllib.parse
+import aiofiles
 import logging
 import aiohttp
 
 
-async def render_page(id, secure_hash, src=None):
-    file = await LazyPrincessBot.get_messages(int(LOG_CHANNEL), int(id))
-    file_data = await get_file_ids(LazyPrincessBot, int(LOG_CHANNEL), int(id))
+async def render_page(id, secure_hash):
+    file_data=await get_file_ids(LazyPrincessBot, int(LOG_CHANNEL), int(id))
     if file_data.unique_id[:6] != secure_hash:
-        logging.debug(f"link hash: {secure_hash} - {file_data.unique_id[:6]}")
+        logging.debug(f'link hash: {secure_hash} - {file_data.unique_id[:6]}')
         logging.debug(f"Invalid hash for message with - ID {id}")
         raise InvalidHash
-
-    src = urllib.parse.urljoin(
-        URL,
-        f"{id}/{urllib.parse.quote_plus(file_data.file_name)}?hash={secure_hash}",
-    )
-
-    tag = file_data.mime_type.split("/")[0].strip()
-    file_size = humanbytes(file_data.file_size)
-    if tag in ["video", "audio"]:
-        template_file = "template/req.html"
+    src = urllib.parse.urljoin(URL, f'{secure_hash}{str(id)}')
+    if str(file_data.mime_type.split('/')[0].strip()) == 'video':
+        async with aiofiles.open('template/req.html') as r:
+            heading = 'Watch {}'.format(file_data.file_name)
+            tag = file_data.mime_type.split('/')[0].strip()
+            html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src)
+    elif str(file_data.mime_type.split('/')[0].strip()) == 'audio':
+        async with aiofiles.open('template/req.html') as r:
+            heading = 'Listen {}'.format(file_data.file_name)
+            tag = file_data.mime_type.split('/')[0].strip()
+            html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src)
     else:
-        template_file = "template/dl.html"
-        async with aiohttp.ClientSession() as s:
-            async with s.get(src) as u:
-                file_size = humanbytes(int(u.headers.get("Content-Length")))
-
-    with open(template_file) as f:
-        template = jinja2.Template(f.read())
-
-    file_name = file_data.file_name.replace("_", " ")
-
-    return template.render(
-        file_name=file_name,
-        file_url=src,
-        file_size=file_size,
-        file_unique_id=file_data.unique_id,
-    )
+        async with aiofiles.open('template/dl.html') as r:
+            async with aiohttp.ClientSession() as s:
+                async with s.get(src) as u:
+                    heading = 'Download {}'.format(file_data.file_name)
+                    file_size = humanbytes(int(u.headers.get('Content-Length')))
+                    html = (await r.read()) % (heading, file_data.file_name, src, file_size)
+    return html
