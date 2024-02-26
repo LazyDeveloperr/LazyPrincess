956763c5279ee77f5bafc27d60ccd6d5e649b266
TheBlackXYZ24 <155623566+TheBlackxyz@users.noreply.github.com>
2024-02-26 07:25:53 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,13 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/TheBlackxyz/BlackHole /Eva
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Eva
+fi
+cd /Eva
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
+
